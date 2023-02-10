915a70c21fa7b0b3bd664218c7582e113ef34c07
TuntematonSotilas <batault_gael@msn.com>
2023-02-10 17:20:17 +0100

    first text

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,27 @@ use bevy::prelude::*;
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("/public/fonts/arial.ttf");
+    let text_style = TextStyle {
+        font,
+        font_size: 60.0,
+        color: Color::WHITE,
+    };
+    let text_alignment = TextAlignment::CENTER;
+    // 2d camera
+    commands.spawn(Camera2dBundle::default());
+    // Demonstrate changing translation
+    commands.spawn((
+        Text2dBundle { 
+            text: Text::from_section("test", text_style.clone())
+                .with_alignment(text_alignment),
+            ..default()
+        },
+    ));
+    
+}
+
 // ------ ------
 //     Start
 // ------ ------
@@ -9,5 +30,6 @@ use wasm_bindgen::prelude::*;
 pub fn start() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
 }
