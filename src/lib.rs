ea73af573a221d028ed7fca1e5781bac63546f07
TuntematonSotilas <batault_gael@msn.com>
2023-02-20 16:45:55 +0100

    map

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,34 @@
 use bevy::prelude::*;
 extern crate wasm_bindgen;
+use bevy_ecs_tilemap::TilemapPlugin;
 use wasm_bindgen::prelude::*;
 
+mod helpers;
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("/public/fonts/arial.ttf");
+    // 2d camera
+    commands.spawn(Camera2dBundle::default());
+    
+    let map_handle: Handle<helpers::tiled::TiledMap> = asset_server.load("/public/maps/map.tmx");
+
+     commands.spawn(helpers::tiled::TiledMapBundle {
+         tiled_map: map_handle,
+         ..Default::default()
+     });
+    
+    /*let font = asset_server.load("/public/fonts/arial.ttf");
     let text_style = TextStyle {
         font,
         font_size: 60.0,
         color: Color::WHITE,
     };
     let text_alignment = TextAlignment::CENTER;
-    // 2d camera
-    commands.spawn(Camera2dBundle::default());
+    
     // Demonstrate changing translation
     commands.spawn((Text2dBundle {
         text: Text::from_section("test", text_style).with_alignment(text_alignment),
         ..default()
-    },));
+    },));*/
 }
 
 // ------ ------
@@ -25,7 +37,22 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 #[wasm_bindgen(start)]
 pub fn start() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins( DefaultPlugins
+            .set(WindowPlugin {
+                window: WindowDescriptor {
+                    title: "Tiled Map Editor Example".to_string(),
+                    ..Default::default()
+                },
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest())
+            /*.set(AssetPlugin {
+                watch_for_changes: true,
+                ..default()
+            }))*/
+        )
+        .add_plugin(TilemapPlugin)
+        .add_plugin(helpers::tiled::TiledMapPlugin)
         .add_startup_system(setup)
         .run();
 }
