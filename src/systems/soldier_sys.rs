6b45084d4111a2fb1c67ccc315a1318af490b9f1
TuntematonSotilas <batault_gael@msn.com>
2023-02-23 18:27:17 +0100

    soldier

--- src/systems/soldier_sys.rs
+++ src/systems/soldier_sys.rs
@@ -4,6 +4,10 @@ pub fn spawn_soliders(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 	commands.spawn(SpriteBundle {
         texture: asset_server.load("/public/sprites/soldier.png"),
+		transform: Transform {
+			translation: Vec3::new(0.,0.,10.),
+			..default()
+		},
         ..default()
     });
 }
