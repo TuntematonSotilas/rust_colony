735df6ec1e82820ca494318c2f077a03fdbf38b5
Gaël <gael.batault@arche-mc2.fr>
2023-05-23 12:16:09 +0200

    ui_select

--- src/systems/ui_select.rs
+++ src/systems/ui_select.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*};
+use bevy::{prelude::*, log};
 use kayak_ui::prelude::{widgets::*, *};
 
 use crate::{components::ui_select::{UiSelectState}, utils::constant::{DARK_RED, LIGHT_RED, BLACK, GREEN}};
@@ -47,15 +47,17 @@ pub fn ui_select(
         };
 
         let pic_human: Handle<Image> = asset_server.load("/public/ui/human.png");
-
+        log::info!("asset_server");
 
         rsx! {
 
             <ElementBundle
                 styles={KStyle{
+                    left: Units::Stretch(1.).into(),
+                    right: Units::Stretch(1.).into(),
                     layout_type: LayoutType::Grid.into(),
-                    grid_rows: vec![Units::Stretch(1.0)].into(),
-                    grid_cols: vec![Units::Stretch(1.0), Units::Pixels(20.0)].into(),
+                    grid_rows: vec![Units::Pixels(25.0)].into(),
+                    grid_cols: vec![Units::Pixels(50.0), Units::Pixels(30.0)].into(),
                     ..default()
                 }}>
                 
@@ -63,7 +65,7 @@ pub fn ui_select(
                     styles={KStyle {
                         row_index: 0.into(),
                         col_index: 0.into(),
-                        border: Edge::from((2.,2.,2.,2.)).into(),
+                        border: Edge::new(1.,0.,1.,1.).into(),
                         border_color: Color::hex(DARK_RED).unwrap().into(),
                         background_color: Color::hex(BLACK).unwrap().into(),
                         ..Default::default()
@@ -75,6 +77,7 @@ pub fn ui_select(
                             left: Units::Stretch(1.).into(),
                             right: Units::Stretch(1.).into(),
                             color: Color::hex(GREEN).unwrap().into(),
+                            font_size: (12.).into(),
                             ..Default::default()
                         }}
                         text={TextProps {
@@ -87,9 +90,9 @@ pub fn ui_select(
                     styles={KStyle {
                         row_index: 0.into(),
                         col_index: 1.into(),
-                        border: Edge::from((2.,2.,2.,2.)).into(),
+                        border: Edge::all(1.).into(),
                         border_color: color.into(),
-                        border_radius: Corner::new(0.,10.,0.,10.).into(),
+                        border_radius: Corner::new(0.,20.,0.,20.).into(),
                         background_color: Color::hex(BLACK).unwrap().into(),
                         ..Default::default()
                     }}
@@ -103,8 +106,8 @@ pub fn ui_select(
                             bottom: Units::Stretch(1.0).into(),
                             left: Units::Stretch(1.0).into(),
                             right: Units::Stretch(1.0).into(),
-                            width: Units::Pixels(10.).into(),
-                            height: Units::Pixels(10.).into(),
+                            width: Units::Pixels(14.).into(),
+                            height: Units::Pixels(14.).into(),
                             ..Default::default()
                         }} />
 
